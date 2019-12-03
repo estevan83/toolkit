47e08f882e16c60b6cfec918f3bb60bbb1d524e9
Estefan Civera <estefan.civera@gmail.com>
2019-12-03 09:03:37 +0100

    Creazione file truncate

---
+++ truncate.php
@@ -0,0 +1,18 @@
+<?php
+
+/* eseguio questa query e creo lo script */
+
+SET @dbname = 'vtigerdb';
+
+SELECT 'SET FOREIGN_KEY_CHECKS = 0;' as schemaresult union
+SELECT
+    CONCAT('DROP TABLE  ', @dbname,'.',TABLE_NAME,';')
+
+FROM
+    information_schema.tables
+
+WHERE
+    table_schema = @dbname
+UNION
+SELECT 'SET FOREIGN_KEY_CHECKS = 1;
+
