7744874698ddf4ace87c8fe56ccd36bb328ca804
Estefan Civera <estefan.civera@gmail.com>
2020-03-04 07:39:23 +0100

    Refactor funnzioni

--- imap.php
+++ imap.php
@@ -1,31 +1,57 @@
+
+<html>
+<body>
+
+<form action="imapTest.php" method="post">
+E-mail: <input type="text" name="email" value="<?php echo (isset($_GET['email']) ? $_GET['email'] : '')?>"><br>
+Password: <input type="password" name="password" value="<?php echo (isset($_GET['password']) ? $_GET['password'] : '')?>"><br>
+Server: <input type="text" name="host" value="<?php echo (isset($_GET['host']) ? $_GET['host'] : '')?>"><br>
+Porta: <input type="text" name="port" value="<?php echo (isset($_GET['port']) ? $_GET['port'] : '')?>"><br>
+<input type="hidden" name="via" value="993">
+<input type="submit">
+</form>
+
+</body>
+</html>
 <?php
-die("UTILIZZARE FILE imapTest.php");
-// phpinfo();
-// die();
-/* connect to gmail */
-/* $hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
-$username = 'colloquiatt@gmail.com';
-$password = 'ttcolloquia2019'; */
+if ((empty($_POST["email"]) && empty($_GET["email"])) || (empty($_POST["password"]) && empty($_GET["password"])) || (empty($_POST["host"]) && empty($_GET["host"])) || (empty($_POST["port"]) && empty($_GET["port"]))){
+	die();
+}
 
-
-// http://testcrm.ribo.it/		imapTest.php?host={imap.gmail.com:993/imap/ssl}INBOX&username=algomatest2@gmail.com&password=Alg0matest
-// imapTest.php?host={10.0.0.13:993}INBOX&username=ribo\crmlead&password=R1b02019!Algoma
-$hostname = $_GET["host"];
-$username = $_GET["username"];
-$password = $_GET["password"];
-
+echo ("PROVA CONNESSIONE ...");
+echo ("<br>");
+	
 /*
-$hostname = '{10.0.0.13:993/imap/ssl/novalidate-cert}INBOX'; // -> Cannot connect to Gmail: [CLOSED] IMAP connection broken (server response) 
-$hostname = '{10.0.0.13:993/imap}INBOX';
-$hostname = '{10.0.0.13:143/imap}INBOX'; 
-$username = 'ribo\crmlead';
-$password = 'R1b02019!Algoma';*/
+$hostname = '{outlook.office365.com:993/imap/ssl}INBOX';
+$username = 'giovanni.ongaro@omca.it';
+$password = 'Benvenuto2019$';
+*/
+$hostname = '{'.$_POST["host"].':'.$_POST["port"].'/imap/ssl}INBOX';     // '{outlook.office365.com:993/imap/ssl}INBOX';
+$username = $_POST["email"];
+$password = $_POST["password"];
+
+echo ("SERVER");
+echo ("<br>");
+echo ($hostname);
+echo ("<br>");
+echo ("USERNAME");
+echo ("<br>");
+echo ($username);
+echo ("<br>");
+echo ("PASSWORD");
+echo ("<br>");
+echo ($password);
+echo ("<br>");
+/*
+$hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
+$username = 'algomacrm@gmail.com';
+$password = 'alg0macrm';*/
 /* try to connect */
-$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
+$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to '.$hostname.': ' . imap_last_error());
 
 /* grab emails */
 $emails = imap_search($inbox,'ALL');
-
+die("<h1>Connesso</h1>");
 /* if emails are returned, cycle through each... */
 if($emails) {
   
@@ -36,9 +62,29 @@ if($emails) {
   rsort($emails);
   
   /* for every email... */
+  foreach($emails as $email_number) {
+	
+	/* get information specific to this email */
+	$overview = imap_fetch_overview($inbox,$email_number,0);
+	$message = imap_fetchbody($inbox,$email_number,2);
+	
+	/* output the email header information */
+	$output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
+	$output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
+	$output.= '<span class="from">'.$overview[0]->from.'</span>';
+	$output.= '<span class="date">on '.$overview[0]->date.'</span>';
+	$output.= '</div>';
+	
+	/* output the email body */
+	$output.= '<div class="body">'.$message.'</div>';
+  }
   
-  echo count($emails);
+  echo $output;
 } 
 
 /* close the connection */
 imap_close($inbox);
+
+
+
+
